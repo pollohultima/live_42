6793ea3f7998610b701398ed45a97e84a0eee941
pollohultima <matteo.polonio@gmail.com>
2021-11-11 09:38:38 +0100

    ok

---
+++ console/first-vue/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <div>
+      <ul>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          iusto reiciendis, velit accusantium inventore facere nesciunt sapiente
+          vel quis maxime!
+        </li>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          iusto reiciendis, velit accusantium inventore facere nesciunt sapiente
+          vel quis maxime!
+        </li>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          iusto reiciendis, velit accusantium inventore facere nesciunt sapiente
+          vel quis maxime!
+        </li>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          iusto reiciendis, velit accusantium inventore facere nesciunt sapiente
+          vel quis maxime!
+        </li>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          iusto reiciendis, velit accusantium inventore facere nesciunt sapiente
+          vel quis maxime!
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
